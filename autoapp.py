2e27c45f0019f4673119c15c8d3a6e09bfa7a142
Phuong Dai <Kanx4.freedom@gmail.com>
2020-08-05 17:17:08 +0700

    Change for WSGI

--- autoapp.py
+++ autoapp.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from flaskshop.app import create_app
+import sys
+
+sys.path.append("/home/phuongdai/Downloads/STU/flask-shop")
 
 app = create_app()
 
